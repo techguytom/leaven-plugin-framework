c162c7b652effe7a810159c8ec0b1827a9bea6ff
Tom Jenkins <tom@thejenkinsweb.com>
2014-04-21 22:48:21 -0500

    Initial Commit

---
+++ bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: TGT Plugin Framework
+ * Plugin URI: http://techguytom.com
+ * Description: Base files for WordPress Plugin Framework
+ * Version: 0.2
+ * Author: Tom Jenkins <tom@techguytom.com>
+ * Author URI: http://techguytom.com
+ */
+
+require_once 'TgtPlugin.php';
+
+$tgtPlugin = new Tgt\TgtPlugin;
+
+$tgtPlugin->setPath(realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
+spl_autoload_register(array($tgtPlugin, 'autoloader'));
+
+register_activation_hook(__FILE__, array($tgtPlugin, 'pluginActivation'));
+register_deactivation_hook(__FILE__, array($tgtPlugin, 'pluginDeactivation'));
+
+add_action('init', array($tgtPlugin, 'init'));
